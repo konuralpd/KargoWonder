620562d1b29e53b64fe6a32404d4e25eb6ef212a
konuralpd <98783085+konuralpd@users.noreply.github.com>
2022-07-23 01:20:44 +0300

    Initial Commit

---
+++ KargoWonder/KargoWonderApp.swift
@@ -0,0 +1,17 @@
+//
+//  KargoWonderApp.swift
+//  KargoWonder
+//
+//  Created by Mac on 23.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct KargoWonderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
